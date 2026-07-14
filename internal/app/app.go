cac0fdbdb463d7005ce41200b8bbccccff116f49
Harish Nagaraju <harishnagaraju.com>
2026-07-14 13:12:38 +0530

    v0.9.0: Refactor application bootstrap entry point

---
+++ internal/app/app.go
@@ -0,0 +1,14 @@
+package app
+
+// App represents the AstraMind application.
+//
+// At the beginning of v0.9.0 it is intentionally empty.
+// Additional dependencies will be added incrementally during
+// the architecture refactoring.
+type App struct {
+}
+
+// New creates a new application instance.
+func New() *App {
+	return &App{}
+}
